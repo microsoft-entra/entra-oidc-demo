3d87ccee56c24eb4d939bc2942f8da0646296c18
Yoel Hor <yoelhor@live.com>
2022-10-09 13:12:15 +0300

    User profile and link to jwt.ms

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
 
 namespace entra_oidc_demo.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    public string AccessToken { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition)
     {
         _logger = logger;
+        _tokenAcquisition = tokenAcquisition;
+        AccessToken = "";
     }
 
-    public void OnGet()
+    public async Task OnGetAsync()
     {
+        if (User.Identity!.IsAuthenticated)
+        {
+            try
+            {
+                this.AccessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "https://graph.microsoft.com/.default" });
+                ViewData["access_token"] = this.AccessToken;
+            }
+            catch (System.Exception)
+            {
 
+            }
+
+        }
     }
 }
