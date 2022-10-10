77106878915717840d058f5b08412d92e1f03228
Yoel Hor <yoelhor@live.com>
2022-10-10 22:55:42 +0300

    Discovery page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Web;
 
@@ -7,30 +7,14 @@ namespace entra_oidc_demo.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly ITokenAcquisition _tokenAcquisition;
-    public string AccessToken { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition)
     {
         _logger = logger;
-        _tokenAcquisition = tokenAcquisition;
-        AccessToken = "";
     }
 
-    public async Task OnGetAsync()
+    public void OnGetAsync()
     {
-        if (User.Identity!.IsAuthenticated)
-        {
-            try
-            {
-                this.AccessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "https://graph.microsoft.com/user.read" });
-                ViewData["access_token"] = this.AccessToken;
-            }
-            catch (System.Exception)
-            {
 
-            }
-
-        }
     }
 }
---
+++ Pages/Me.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
+
+namespace entra_oidc_demo.Pages;
+
+public class MeModel : PageModel
+{
+    private readonly ILogger<MeModel> _logger;
+    private readonly ITokenAcquisition _tokenAcquisition;
+    public string AccessToken { get; set; }
+
+    public MeModel(ILogger<MeModel> logger, ITokenAcquisition tokenAcquisition)
+    {
+        _logger = logger;
+        _tokenAcquisition = tokenAcquisition;
+        AccessToken = "";
+    }
+
+    public async Task OnGetAsync()
+    {
+        if (User.Identity!.IsAuthenticated)
+        {
+            try
+            {
+                this.AccessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "https://graph.microsoft.com/user.read" });
+                ViewData["access_token"] = this.AccessToken;
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+        }
+    }
+}
