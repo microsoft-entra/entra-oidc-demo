4a7a1bf3f19be7f3c85753f0d0fedc3b45548f28
Yoel Hor <yoelhor@live.com>
2022-10-18 14:46:36 +0300

    App roles

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Web;
@@ -9,6 +11,8 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private readonly ITokenAcquisition _tokenAcquisition;
     public string AccessToken { get; set; }
+    public bool ShowEdit { get; set; }
+
 
     public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition)
     {
@@ -21,8 +25,25 @@ public class IndexModel : PageModel
     {
         try
         {
-            this.AccessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "https://graph.microsoft.com/user.read" });
+            this.AccessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "api://4ec7fcdd-1ef9-47fc-a60c-9ef3ac716cb9/Read" });
             ViewData["access_token"] = this.AccessToken;
+
+
+            var handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtSecurityToken = handler.ReadJwtToken(this.AccessToken);
+
+            List<Claim> roles = jwtSecurityToken.Claims
+                .Where(x => x.Type == "roles").ToList<Claim>();
+
+            foreach (Claim role in roles)
+            {
+                if (roles != null && (role.Value.ToLower().Contains("headteacher")))
+                {
+                    ShowEdit = true;
+                    break;
+                }
+            }
+
         }
         catch (System.Exception ex)
         {
